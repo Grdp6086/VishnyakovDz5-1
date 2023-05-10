7b7b07c256d87c984b972eca680fcc78b03d82ac
Dmitry Vishniakov <112121dfd@gmail.com>
2023-05-10 17:46:15 +0700

    Change tests

--- src/test/java/ru/netology/javaqa/services/DayOffServiceTest.java
+++ src/test/java/ru/netology/javaqa/services/DayOffServiceTest.java
@@ -9,22 +9,12 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class DayOffServiceTest {
 
-    @Test
-    void testFirstEx() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/dayOff.csv")
+    public void test(int expected, int income, int expenses, int threshold) {
         DayOffService service = new DayOffService();
 
-        int expected = 3;
-        int actual = service.calculate(10000, 3000, 20000);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void testSecondEx() {
-        DayOffService service = new DayOffService();
-
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
